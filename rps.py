8adc44b4720667e5495ccc609ad29fffcc53a0ed
andreasmalta1 <38852990+andreasmalta1@users.noreply.github.com>
2021-07-31 14:14:40 +0200

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,21 +1,28 @@
+# Rock paper scissors game against a random computer
+# Choose "r", "s" or "p" to play
+
 import random
 
 
 def play():
+    # Getting the user input
     user = input("Type 'r' for rock, 'p' for paper, 's': ").lower()
+    # Computer generates a random selection
     computer = random.choice(["r", "p", "s"])
 
+    # If selections are equal this match is a tie
     if user == computer:
         return "It's a tie"
 
-    if is_win(user,computer):
+    if is_win(user, computer):
         return "You won"
 
     return "You lost"
 
 
-def is_win(player, opponent):
-    if (player == "r" and opponent == "s") or (player == "s" and opponent == "p") or (player == "p" and opponent == "r"):
+# Choosing a winner
+def is_win(player, robot):
+    if (player == "r" and robot == "s") or (player == "s" and robot == "p") or (player == "p" and robot == "r"):
         return True
 
 
